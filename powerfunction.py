54168e7f0f3852eabdb3dbf1e8c7b7e1a4dea964
Zach <ztrichas2019@pingry.org>
2016-04-12 15:14:06 -0400

    Only functions in the file now

--- powerfunction.py
+++ powerfunction.py
@@ -1,22 +1,24 @@
-#Call on the fucntion to keep doing it 
-
-#Write tests and make sure function is recursive
-#Write a main program that uses a recursive int_input and float input
+def int_input(prompt):
+	answer = raw_input(prompt)
+	
+	try:
+		return int(answer)
+	except ValueError:
+		return int_input("That wasn't a number. Try again.")
+		
+def float_input(prompt):
+	answer = raw_input(prompt)
+	
+	try:
+		return float(answer)
+	except ValueError:
+		return int_input("That wasn't a number. Try again.")
 
 def recursivePowerFunction(base, exponent):
-	#if base == 0:
-	#	return 0
+
 	if exponent == 0:
 		return 1 
 		
-	#if exponent == 1: #Terminating case
-	#	return base
 	return base * recursivePowerFunction(base, exponent - 1)
+	
 
-#Create a main program with the float input and int input 
-#Ask tom about the int input function
-
-base = float_input("Input a base.")
-exponent = int_input("Input an exponent.")
-
-print( recursivePowerFunction(base, exponent))
