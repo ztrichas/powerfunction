16b8ca0f85edda590e6cb668d8adad505a4affa3
Zach <ztrichas2019@pingry.org>
2016-04-12 15:16:07 -0400

    Initial Commit for the Test cases for the program

---
+++ powerFunctionTestCases.py
@@ -0,0 +1,14 @@
+import recursivePowerFunction
+
+print (recursivePowerFunction(3, 5))
+
+print (recursivePowerFunction(4, 7))
+
+print (recursivePowerFunction(1, 0))
+
+print (recursivePowerFunction(0, 4))
+
+print (recursivePowerFunction(3.5, 3))
+
+print (recursivePowerFunction(5, 1))
+
